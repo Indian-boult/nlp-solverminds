6ffc4fd873ae7d302bbe28203d3b8bbc1161c69f
askaydevs <sk_singh18@outlook.com>
2020-02-26 12:45:12 +0530

    Code docs.

--- basic_prepro.py
+++ basic_prepro.py
@@ -4,17 +4,31 @@ import nltk
 from nltk.corpus import stopwords
 import spacy
 
-def process(text):                                                                      # Input type: str
-    processed = text.replace('\n', ' ').replace('\r', '')                               # New line and carriage return removal
-    processed = re.sub('\d\.\s+|[a-z]\)\s+|•\s+|[A-Z]\.\s+|[IVX]+\.\s+', ' ', text)     # Bullets & numbering removal
-    processed = re.sub(r'\s+', ' ', processed)                                          # Continuous sapce removal
-    processed = re.sub(r'[^\w\s]','',processed)                                         # Punctuation & special chars removal
-    processed = re.sub("\d+", " ", processed)                                           # Numbers removal
-    processed = processed.lower()                                                       # Case conversion
-    processed = processed.strip()
-    return processed                                                                    # Output type: str
+def process(text):
+    """
+    Clean a string for newlines, bullets, numbering, chunk of spaces,
+    punctuation and, convert the string to lowercase
 
-def do_tokeinze(text, tool):                                                                  # Tokenization
+    :param str text: The string to be processed, passed a an python string
+    :return str processed: processed text.
+    """
+    processed = text.replace('\n', ' ').replace('\r', '')
+    processed = re.sub('\d\.\s+|[a-z]\)\s+|•\s+|[A-Z]\.\s+|[IVX]+\.\s+', ' ', text)
+    processed = re.sub(r'\s+', ' ', processed)
+    processed = re.sub(r'[^\w\s]','',processed)
+    processed = re.sub("\d+", " ", processed)
+    processed = processed.lower()
+    processed = processed.strip()
+    return processed
+
+def do_tokeinze(text, tool='nltk'):
+    """
+    The native nltk string tokenizer with support for spacy.
+
+    :param str text: The string to be tokenized, passed a as python string
+    :param str tool: Tool to be used for tokenization. (default 'nltk')
+    :return list processed: tokenized text
+    """
     if tool == 'spacy':
         nlp = spacy.load('en_core_web_sm')
         doc = nlp(text)
@@ -25,14 +39,18 @@ def do_tokeinze(text, tool):                                                    
     elif tool == 'nltk':
         return nltk.tokenize.word_tokenize(text)
 
-def rm_stopwords(text):                                                                 # Input type: str
-    eng_stopwords = set(stopwords.words('english'))                                     # Reading english stopwords
-    data = process(text)                                                                # Basic data preprocessing
-    data_tokenized = do_tokeinze(data)                                                  # Tokenization
-    data_wo_stpwrds = []
-    for token in data_tokenized:                                                        # Stopwords removal
-        if token not in(eng_stopwords):
-            data_wo_stpwrds.append(token)
+def rm_stopwords(token_list):
+    """
+    function to remove stopwords as specified in nltk english stopwords from a
+    list of tokens.
 
-    return data_wo_stpwrds                                                              # Returns tokenized data without stopwords
-                                                                                            # Output type: list
+    :param list token_list: list of tokens to be filtered
+    :return list clean_token_list: filtered list of tokens (no stopwords)
+    """
+    eng_stopwords = set(stopwords.words('english'))
+    clean_token_list = []
+    for token in token_list:
+        if token not in(eng_stopwords):
+            clean_token_list.append(token)
+
+    return clean_token_list
