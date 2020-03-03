d0f0d6331e82c15f1c070322204f31621f07bd20
askaydevs <sk_singh18@outlook.com>
2020-03-03 11:34:49 +0530

    keyword extraction using text rank.

---
+++ terminology_extraction/example_terminlogy.py
@@ -0,0 +1,9 @@
+from text_rank import TextRank4Keyword
+
+text = '''
+The Wandering Earth, described as China’s first big-budget science fiction thriller, quietly made it onto screens at AMC theaters in North America this weekend, and it shows a new side of Chinese filmmaking — one focused toward futuristic spectacles rather than China’s traditionally grand, massive historical epics. At the same time, The Wandering Earth feels like a throwback to a few familiar eras of American filmmaking. While the film’s cast, setting, and tone are all Chinese, longtime science fiction fans are going to see a lot on the screen that reminds them of other movies, for better or worse.
+'''
+
+tr4w = TextRank4Keyword()
+tr4w.analyze(text, candidate_pos = ['NOUN', 'PROPN'], window_size=4, lower=False)
+tr4w.get_keywords(10)
