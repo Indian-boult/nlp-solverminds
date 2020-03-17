af67f738bca6412ea34e720d5c751aab1bcd76c8
askaydevs <sk_singh18@outlook.com>
2020-03-17 17:40:11 +0530

    Removed inconsistent

--- feature_extraction/n_grams/example_ngrams.py
+++
@@ -1,7 +0,0 @@
-from Ngrams import ngrams
-
-n = 2
-n_gen = ngrams('Better problem definitions often lead to better solutions.'.split(), n)
-
-n_gram_list = list(n_gen)
-print(n_gram_list)
--- read_file/example.py
+++
@@ -1,6 +0,0 @@
-from reader import get_data
-from basic_prepro import process, do_tokeinze
-text, title = get_data('001.pdf')
-
-processed = process(text)
-print(len(do_tokeinze(processed)))
